a681776c65e52844a7ef4ffdbd6aa6a9dc1e8a44
pantelis <15629182+pampatzoglou@users.noreply.github.com>
2025-01-03 14:17:16 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,44 +13,5 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin, ... }:
-    let
-      # Common modules shared between NixOS and Darwin
-      commonModules = [
-      #  ./common/base.nix
-      #  ./common/personal.nix
-      #  ./common/infrastructure.nix
-      #  ./common/kubernetes.nix
-      #  ./common/development.nix
-      #  ./common/monitoring.nix
-      #  ./common/network.nix
-      #  ./common/security.nix
-      ];
-    in {
-      # NixOS configuration
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixos/configuration.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pantelis = import ./common/home.nix;
-          }
-        ] ++ commonModules;
-      };
 
-      # Darwin (macOS) configuration
-      darwinConfigurations.default = darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        modules = [
-          ./darwin/configuration.nix
-          home-manager.darwinModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pantelis = import ./common/home.nix;
-          }
-        ] ++ commonModules;
-      };
-    };
 }
